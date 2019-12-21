bc9dead2ebb8351d94205ffc98f16069660f4c24
rickyrattlesnake <ricky.ratnayake@gmail.com>
2019-12-21 11:07:45 +1100

    wip: only mac support for zsh

--- zsh/apply.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/zsh
-cp ./.mac.zshrc ~/.zshrc
-cp ./.mac.zshrc.keybinding ~/.zshrc.keybinding
-cp ./.zsh_plugins.sh ~/.zsh_plugins.sh
-cp ./.mac.zshrc.alias ~/.zshrc.alias
---
+++ zsh/mac/apply.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/zsh
+cp ./.zshrc ~/.zshrc
+cp ./.zshrc.keybinding ~/.zshrc.keybinding
+cp ./.zsh_plugins.sh ~/.zsh_plugins.sh
+cp ./.zshrc.alias ~/.zshrc.alias
