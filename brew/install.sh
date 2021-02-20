2819a6e7e5e687aacfe34736133a2a58b15992ff
rickyrattlesnake <ricky.ratnayake@gmail.com>
2021-02-20 13:24:24 +1100

    brew

--- brew/install.sh
+++ brew/install.sh
@@ -3,36 +3,85 @@
 brew update
 brew tap homebrew/cask-fonts
 
-brew install virtualbox \
-  gpg \
-  zsh \
-  python3 \
-  python2 \
-  ag \
-  p7zip \
-  jq \
-  yq \
-  kubernetes-cli \
-  rbenv \
-  lastpass-cli \
-  terraform \
-  tree \
+brew install \
+  ansible \
+  antibody \
+  automake \
+  aws-okta \
+  awscli \
+  bufbuild/buf/buf \
+  cmake \
+  fd \
+  ffmpeg \
   fzf \
-  fd
+  git \
+  gnupg \
+  go \
+  gpg \
+  graphviz \
+  guetzli \
+  imagemagick \
+  jmeter \
+  jq \
+  kubernetes-cli \
+  lastpass-cli \
+  mitmproxy \
+  newman \
+  nmap \
+  nvm \
+  p7zip \
+  postgresql \
+  python@2 \
+  python2 \
+  python3 \
+  rbenv \
+  rename \
+  rustup-init \
+  stern \
+  terraform \
+  terraform-docs \
+  tfenv \
+  the_silver_searcher \
+  tree \
+  unrar \
+  watch \
+  watchman \
+  yarn \
+  yq \
+  zsh
 
-brew install --cask font-hack-nerd-font \
-  iterm2 \
-  visual-studio-code \
-  gpg-suite-no-mail \
-  meld \
-  intellij-idea \
-  font-fira-code \
+brew install --cask \
   1password \
-  spotify \
-  postman \
+  aws-vpn-client \
+  balenaetcher \
+  bloomrpc \
+  calibre \
+  charles \
   datagrip \
+  firefox \
+  font-fira-code \
+  font-hack-nerd-font \
+  google-chrome \
+  gpg-suite \
+  gpg-suite-no-mail \
+  intellij-idea \
+  iterm2 \
+  jprofiler \
+  kap \
+  keka \
+  mplayerx \
+  ngrok \
+  postman \
+  signal \
+  spectacle \
+  spotify \
+  transmission \
   tunnelblick \
-  signal
+  virtualbox \
+  visual-studio-code \
+  vlc \
+  wireshark \
+  xee
 
 # AWS
 pip3 install awscli --upgrade --user
