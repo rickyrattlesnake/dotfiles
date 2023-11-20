808f7c924c876d0320dc71b826e37eb84de1feb5
rickyrattlesnake <ricky.ratnayake@gmail.com>
2023-11-20 13:02:30 +1100

    wip:fiz vscode setup

--- ansible/assets/fish/configs/config.fish
+++ ansible/assets/fish/configs/config.fish
@@ -28,10 +28,17 @@ if test -d /opt/homebrew/bin
     fish_add_path -p /opt/homebrew/bin
 end
 
+if test -d $HOME/Library/Python/3.9/bin
+    fish_add_path -p $HOME/Library/Python/3.9/bin
+end
+
+if test -d $HOME/.cargo/bin
+    fish_add_path -p $HOME/.cargo/bin
+end
+
 ## Set aws ca certificate to mozilla when installed by brew
 set -gx AWS_CA_BUNDLE $(brew --prefix)/etc/ca-certificates/cert.pem
 
-fish_add_path $HOME/.cargo/bin
 # fish_add_path /usr/local/opt/tcl-tk/bin
 # fish_add_path /usr/local/opt/llvm/bin
 set -gx VOLTA_HOME "$HOME/.volta"
